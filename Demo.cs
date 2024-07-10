10728290357174ce5a6fca8883c2ce9b0a6484b8
SanthoshHB <sanhb@deloitte.com>
2024-07-10 17:33:23 +0530

    Demo.cs File Committed

---
+++ Demo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Demo1
+{
+    internal class Demo
+    {
+        public static int staticCount;
+        public  int intCount; 
+
+        public void LocalVariableDemo()
+        {
+            int count=100; //No Default value --Value assigned before using them
+            Console.WriteLine("Counts : {0}", count);
+        }
+        public void Print()
+        {
+           
+            staticCount++;
+            intCount++;
+            Console.WriteLine("Static : {0}   Integer :{1} ",staticCount, intCount);
+
+        }
+        public void PrintDemo()
+        {
+            staticCount++;
+            intCount++;
+            Console.WriteLine("Static : {0}   Integer :{1} ", staticCount, intCount);
+        }
+    }
+}
